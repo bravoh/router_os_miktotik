492088767150d6f24ed2cbbfbd7de1f4596dcb39
CharlesKamonde <84808670+CharlesKamonde@users.noreply.github.com>
2021-06-16 23:50:06 +0300

    Update router_os.php

--- config/router_os.php
+++ config/router_os.php
@@ -28,10 +28,22 @@ return [
             'limit-at'=>"10M/10M",
             'type'=>'test'
         ),
+        '1000.00' => array(
+            'name'=>"2mbps",
+            'max-limit'=>"2M/2M",
+            'limit-at'=>"2M/2M",
+            'type'=>'live'
+        ),
         '1500.00' => array(
             'name'=>"3mbps",
             'max-limit'=>"3M/3M",
             'limit-at'=>"3M/3M",
+            'type'=>'live'
+        ),
+        '2000.00' => array(
+            'name'=>"4mbps",
+            'max-limit'=>"4M/4M",
+            'limit-at'=>"4M/4M",
             'type'=>'live'
         ),
         '2500.00' => array(
@@ -40,7 +52,7 @@ return [
             'limit-at'=>"5M/5M",
             'type'=>'live'
         ),
-        '5000' => array(
+        '4000' => array(
             "name"=>"10mbps",
             'max-limit'=>"10M/10M",
             'limit-at'=>"10M/10M",
