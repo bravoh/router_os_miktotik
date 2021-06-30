bcb8744cc96612f928a53aa84c03a459c7c5a6fb
CharlesKamonde <84808670+CharlesKamonde@users.noreply.github.com>
2021-06-30 19:38:12 +0300

    Add files via upload

---
+++ app/Http/Controllers/SmsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sms;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SmsController extends Controller
+{
+    public function index(){
+        $sms = Sms::orderBy('id','desc')->get();
+        return view('sms.index',compact('sms'));
+    }
+
+}
